9936eea0143179acd81573d083ecd27b03947573
vuong73 <nguyenquocvuong27042002@gmail.com>
2023-11-14 00:33:20 +0700

    GiaoDienHoanThien

--- Du_An_1_Nhom_6 (1)/src/com/raven/form/Form_Login.java
+++ Du_An_1_Nhom_6 (1)/src/com/raven/form/Form_Login.java
@@ -4,7 +4,12 @@
  */
 package com.raven.form;
 
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.Timer;
 
 /**
  *
@@ -16,8 +21,12 @@ public class Form_Login extends javax.swing.JDialog {
 
     public Form_Login(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
+        runTieuDe();
+          setSize(1180, 657);
+        setLocationRelativeTo(null);
         this.cc = parent;
         initComponents();
+
     }
 
     void ketThuc() {
@@ -26,6 +35,36 @@ public class Form_Login extends javax.swing.JDialog {
             System.exit(0);
         }
 
+    }
+    private Timer timer;
+    private int posX;
+
+    private void runTieuDe() {
+        lable = new JLabel("Phần Mềm Bán Áo Phông Bee-Shirt");
+
+        lable.setFont(new Font("Arial", Font.PLAIN, 20));
+
+        posX = getContentPane().getWidth();
+
+        timer = new Timer(10, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                posX--;
+
+                lable.setLocation(posX, lable.getY());
+
+                if (posX + lable.getWidth() <= 0) {
+                    posX = getContentPane().getWidth() - lable.getWidth();
+                }
+            }
+        });
+
+        getContentPane().setLayout(null);
+        getContentPane().add(lable);
+
+        pack();
+
+        timer.start();
     }
 
     /**
@@ -49,11 +88,13 @@ public class Form_Login extends javax.swing.JDialog {
         myButton1 = new swing_Login.MyButton();
         myButton2 = new swing_Login.MyButton();
         jLabel4 = new javax.swing.JLabel();
+        lable = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         panelGradiente1.setColorPrimario(new java.awt.Color(146, 233, 251));
         panelGradiente1.setColorSecundario(new java.awt.Color(12, 137, 163));
+        panelGradiente1.setPreferredSize(new java.awt.Dimension(1180, 657));
 
         panelBorder1.setBackground(new java.awt.Color(255, 255, 255));
 
@@ -102,10 +143,12 @@ public class Form_Login extends javax.swing.JDialog {
                     .addComponent(jLabel3)
                     .addComponent(jLabel1)
                     .addComponent(txtUser, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 257, Short.MAX_VALUE)
                     .addComponent(txtPass, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(myButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(myButton2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 257, Short.MAX_VALUE))
+                    .addComponent(myButton2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 257, Short.MAX_VALUE)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(48, 48, 48)
+                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(44, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -128,11 +171,18 @@ public class Form_Login extends javax.swing.JDialog {
                 .addContainerGap(11, Short.MAX_VALUE))
         );
 
+        jLabel4.setForeground(new java.awt.Color(153, 153, 153));
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/raven/icon/3.png"))); // NOI18N
+        jLabel4.setText("Bee-Shirt");
+
         javax.swing.GroupLayout slideLayout = new javax.swing.GroupLayout(slide);
         slide.setLayout(slideLayout);
         slideLayout.setHorizontalGroup(
             slideLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 373, Short.MAX_VALUE)
+            .addGroup(slideLayout.createSequentialGroup()
+                .addGap(133, 133, 133)
+                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(155, Short.MAX_VALUE))
             .addGroup(slideLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(slideLayout.createSequentialGroup()
                     .addGap(0, 0, Short.MAX_VALUE)
@@ -141,7 +191,10 @@ public class Form_Login extends javax.swing.JDialog {
         );
         slideLayout.setVerticalGroup(
             slideLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 454, Short.MAX_VALUE)
+            .addGroup(slideLayout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(405, Short.MAX_VALUE))
             .addGroup(slideLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(slideLayout.createSequentialGroup()
                     .addGap(0, 0, Short.MAX_VALUE)
@@ -168,43 +221,42 @@ public class Form_Login extends javax.swing.JDialog {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        jLabel4.setBackground(new java.awt.Color(102, 102, 102));
-        jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
-        jLabel4.setForeground(new java.awt.Color(102, 102, 102));
-        jLabel4.setText("Phần Mềm Bán Áo Phông Bee-Shirt");
+        lable.setBackground(new java.awt.Color(102, 102, 102));
+        lable.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
+        lable.setForeground(new java.awt.Color(102, 102, 102));
+        lable.setText("Phần Mềm Bán Áo Phông Bee-Shirt");
 
         panelGradiente1.setLayer(panelBorder1, javax.swing.JLayeredPane.DEFAULT_LAYER);
-        panelGradiente1.setLayer(jLabel4, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        panelGradiente1.setLayer(lable, javax.swing.JLayeredPane.DEFAULT_LAYER);
 
         javax.swing.GroupLayout panelGradiente1Layout = new javax.swing.GroupLayout(panelGradiente1);
         panelGradiente1.setLayout(panelGradiente1Layout);
         panelGradiente1Layout.setHorizontalGroup(
             panelGradiente1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(panelGradiente1Layout.createSequentialGroup()
-                .addGroup(panelGradiente1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(panelGradiente1Layout.createSequentialGroup()
-                        .addGap(497, 497, 497)
-                        .addComponent(panelBorder1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(panelGradiente1Layout.createSequentialGroup()
-                        .addGap(383, 383, 383)
-                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 628, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(469, Short.MAX_VALUE))
+                .addGap(388, 388, 388)
+                .addComponent(panelBorder1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelGradiente1Layout.createSequentialGroup()
+                .addContainerGap(285, Short.MAX_VALUE)
+                .addComponent(lable, javax.swing.GroupLayout.PREFERRED_SIZE, 628, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(282, 282, 282))
         );
         panelGradiente1Layout.setVerticalGroup(
             panelGradiente1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelGradiente1Layout.createSequentialGroup()
-                .addGap(36, 36, 36)
-                .addComponent(jLabel4)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 74, Short.MAX_VALUE)
+                .addGap(50, 50, 50)
+                .addComponent(lable)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 50, Short.MAX_VALUE)
                 .addComponent(panelBorder1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(121, 121, 121))
+                .addGap(43, 43, 43))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(panelGradiente1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(panelGradiente1, javax.swing.GroupLayout.DEFAULT_SIZE, 1195, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -275,6 +327,7 @@ public class Form_Login extends javax.swing.JDialog {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel lable;
     private swing_Login.MyButton myButton1;
     private swing_Login.MyButton myButton2;
     private swing_Login.PanelBorder panelBorder1;
