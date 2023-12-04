a4efb48613005fdd692f9a05fe20803983161787
DESKTOP-VFRGIH8\MSI <dotrungdang01@gmail.com>
2023-12-04 11:03:53 +0700

    auth

--- Du_An_1_Nhom_6 (1)/src/Utils/Auth.java
+++ Du_An_1_Nhom_6 (1)/src/Utils/Auth.java
@@ -2,23 +2,41 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Utils;
+package utils;
 
 import com.raven.model.Model_NhanVien;
 
+
 /**
  *
- * @author admin
+ * @author ACER
  */
 public class Auth {
     public static Model_NhanVien user=null;
+    
     public static void clear(){
         Auth.user=null;
     }
+    
     public static boolean isLogin(){
         return Auth.user!=null;
     }
+    
+    public static int idNhanVien(){
+        return user.getIdNhanVien();
+    }
+    
+    public static String maNhanVien(){
+        return user.getMaNhanVien();
+    }
+    public static String tenNhanVien(){
+        return user.getHoTen();
+    }
+    public static Boolean ChucVuNhanVien(){
+        return user.isChuVu();
+    }
+    
     public static boolean isManager(){
-      return Auth.isLogin() && user.isChuVu();
+        return Auth.isLogin() && user.isChuVu();
     }
 }
